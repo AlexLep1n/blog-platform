05e1f07d2a77fb07e6a1567705fa85b36a2b35ca
Alex Lepin <alexlepin@mail.ru>
2024-10-27 14:33:58 +0300

    feat: implement get articles by rtkq (need fix for pagination)

--- src/modules/articles/Article/Article.jsx
+++ src/modules/articles/Article/Article.jsx
@@ -1,31 +1,55 @@
+/* eslint-disable react/prop-types */
+import { format } from 'date-fns';
 import classes from './Article.module.css';
-import heart from './assets/heart.svg';
-import userIcon from './assets/user-icon.svg';
+// import heart from './assets/heart.svg';
+// import userIcon from './assets/user-icon.svg';
+import { Rate } from 'antd';
+import { HeartFilled } from '@ant-design/icons';
+import { useMemo } from 'react';
+import { nanoid } from 'nanoid';
 
-export default function Article() {
+export default function Article({
+  author: { username, image: imgUrl },
+  title,
+  description,
+  createdAt,
+  favoritesCount,
+  tagList = [],
+}) {
+  const tagsWithIds = useMemo(() => tagList.map((tag) => ({ name: tag, id: nanoid() })), [tagList]);
+
   return (
     <div className={classes.article}>
       <div className={classes.article__content}>
         <div className={classes['article__title-box']}>
-          <h3 className={classes.article__title}>Some article title</h3>
-          <img className={classes.article__like} src={heart} alt="heart icon" />
-          <span className={classes['article__like-count']}>12</span>
+          <h3 className={classes.article__title}>{title}</h3>
+          <Rate
+            className={classes.article__like}
+            style={{ color: 'red' }}
+            character={<HeartFilled />}
+            count={1}
+            disabled
+          />
+          {/* <img className={classes.article__like} src={heart} alt="heart icon" /> */}
+          <span className={classes['article__like-count']}>{favoritesCount}</span>
         </div>
         <div className={classes.article__tags}>
-          <p className={classes.article__tag}>Tag1</p>
+          {tagsWithIds.map((tag) => (
+            <p key={tag.id} className={classes.article__tag}>
+              {tag.name}
+            </p>
+          ))}
         </div>
-        <p className={classes.article__description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <p className={classes.article__description}>{description}</p>
       </div>
       <div className={classes.article__user}>
         <div className={classes['article__user-info']}>
-          <p className={classes['article__user-name']}>John Doe</p>
-          <p className={classes['article__post-time']}>March 5, 2020 </p>
+          <p className={classes['article__user-name']}>{username}</p>
+          <p className={classes['article__post-time']}>
+            {format(new Date(createdAt), 'MMMM d, yyyy')}
+          </p>
         </div>
-        <img className={classes['article__user-icon']} src={userIcon} alt="user icon" />
+        <img className={classes['article__user-icon']} src={imgUrl} alt="user icon" />
       </div>
     </div>
   );
--- src/modules/articles/Articles/ArticlesList.jsx
+++ src/modules/articles/Articles/ArticlesList.jsx
@@ -1,14 +1,42 @@
+/* eslint-disable no-unused-vars */
 import classes from './Articles.module.css';
 import Article from '../Article/Article';
 import { useGetArticlesQuery } from '../api';
+import { nanoid } from 'nanoid';
+import { useState, useMemo } from 'react';
+import { Pagination } from 'antd';
 
 export default function Articles() {
-  const { data: articles } = useGetArticlesQuery();
-  console.log(articles);
+  const [page, setPage] = useState(1);
+  const [skip, setSkip] = useState(0);
+  console.log(page, skip);
+  const { data, isLoading, isSuccess, isError } = useGetArticlesQuery(page, skip);
+
+  const articles = useMemo(() => data?.articles || [], [data]);
+  const articlesCount = useMemo(() => data?.articlesCount || 5, [data]);
+
+  const articlesWithIds = useMemo(
+    () => articles.map((article) => ({ ...article, id: nanoid() })),
+    [articles]
+  );
 
   return (
     <section className={classes.users}>
-      <Article />
+      {isLoading && <h1>Loading...</h1>}
+      {isSuccess && articlesWithIds.map((article) => <Article key={article.id} {...article} />)}
+      <Pagination
+        current={page}
+        pageSize={5}
+        total={articlesCount}
+        onChange={(page) => {
+          if (page === 1) {
+            setSkip(0);
+          }
+          setSkip(page - 1);
+          setPage(page);
+        }}
+        showSizeChanger={false}
+      />
     </section>
   );
 }
